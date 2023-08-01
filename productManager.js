a4c58251afbde8fd7d582d42b926f0ad148f3a24
CamilaEbis <camiebis@gmail.com>
2023-08-01 20:50:25 -0300

    desafio 2

--- productManager.js
+++ productManager.js
@@ -1,8 +1,12 @@
+import {promises as fs} from 'fs';
 
 class ProductManager{
     constructor() {
         this.products = [];
+        this.path = "Products.txt";
+    
     }
+
 
     addProduct(product) {
         if (this.validateCode(product.code)) {
@@ -10,17 +14,52 @@ class ProductManager{
         } else {
             const producto = {id:this.generateId(), title:product.title, description:product.description, price:product.price, thumbnail:product.thumbnail, code:product.code, stock:product.stock};
             this.products.push(producto);
+            this.saveProducts();
             console.log("Producto agregado!");
         }
     }
 
-    getProducts() {
-        return this.products;
+    updateProduct(id, product) {
+        this.products = this.getProducts();
+        let indice = this.products.findIndex(item => item.id === id);
+
+        if (indice > -1) {
+            this.products[indice].title = product.title;
+            this.products[indice].description = product.description;
+            this.products[indice].price = product.price;
+            this.products[indice].thumbnail = product.thumbnail;
+            this.products[indice].code = product.code;
+            this.products[indice].stock = product.stock;
+            this.saveProducts();
+            console.log("Product updated!");
+        } else {
+            console.log("Producto no encontrado!");
+        }
     }
 
-    getProductById(id) {
-        return this.products.find(item => item.id === id) || "Not found";
+    deleteProduct(id) {
+        this.products = this.getProducts();
+        let indice = this.products.findIndex(item => item.id === id);
 
+        if (indice > -1) {
+            this.products.splice(indice, 1); (0,1)
+            this.saveProducts();
+            console.log("Product #" + id + " deleted!");
+        } else {
+            console.log("Not found!");
+        }
+    }
+
+    async getProducts() {
+        let products = JSON.parse(await fs.readFile(this.path, "utf-8"));
+
+        return products;
+    }
+
+    async getProductById(id) {
+        this.products = JSON.parse(await fs.readFile(this.path, "utf-8"));
+
+        return this.products.find(item => item.id === id) || "Producto no existe";
     }
 
     validateCode(code) {
@@ -38,6 +77,10 @@ class ProductManager{
 
         return max+1;
     }
+
+    async saveProducts() {
+        await fs.writeFile(this.path, JSON.stringify(this.products));
+    }
 }
 
 const test = new ProductManager();
@@ -45,7 +88,7 @@ const test = new ProductManager();
 function myFunction() {
     const objPrducto = {
         title: 'producto prueba',
-        code: 'abc123',
+        code: 123456,
         description: 'Este es un producto prueba',
         price: 200,
         thumbnail: 'sin imagen',
@@ -59,9 +102,7 @@ function myFunction() {
     test.addProduct(objPrducto);
     console.log(test.getProducts());
     test.addProduct(objPrducto);
-
     console.log(test.getProductById(1));
-
 }
 
 myFunction()
